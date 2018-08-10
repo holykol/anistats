a67c901ea4e091c015f70ac1f47389201a3b13e3
Timofey  Barinov <tbarinov@outlook.com>
2018-08-10 19:37:00 +0300

    .env

--- client/poi.config.js
+++ client/poi.config.js
@@ -1,26 +1,33 @@
 const path = require('path')
 const pkg = require('./package')
+const webpack = require('webpack')
+
+require('dotenv').config({path: '../.env'})
 
 module.exports = {
-  entry: [
-    'src/polyfills.js',
-    'src/index.js'
-  ],
-  html: {
-    title: pkg.productName,
-    description: pkg.description,
-    template: path.join(__dirname, 'index.ejs')
-  },
-  postcss: {
-    plugins: [
-      // Your postcss plugins
-    ]
-  },
-  presets: [
-    require('poi-preset-bundle-report')(),
-    require('poi-preset-offline')({
-      pwa: './src/pwa.js', // Path to pwa runtime entry
-      pluginOptions: {} // Additional options for offline-plugin
-    })
-  ]
+   entry: [
+      'src/polyfills.js',
+      'src/index.js'
+   ],
+   html: {
+      title: pkg.productName,
+      description: pkg.description,
+      template: path.join(__dirname, 'index.ejs')
+   },
+   postcss: {
+      plugins: [
+         // Your postcss plugins
+      ]
+   },
+   presets: [
+      require('poi-preset-bundle-report')(),
+      require('poi-preset-offline')({
+         pwa: './src/pwa.js', // Path to pwa runtime entry
+         pluginOptions: {} // Additional options for offline-plugin
+      })
+   ],
+   define : {
+      'process.env.NODE_ENV' : `"${process.env.NODE_ENV || 'development'}"`,
+      'process.env.SIMPERIUM_APP_ID': JSON.stringify(process.env.SIMPERIUM_APP_ID),
+   },
 }
